4e9ec02327853bee0820d67778df8294e453492f
alpana <cprabha2005@gmail.com>
2024-06-02 11:49:27 +0530

    JpaSpecification for complex queries

--- src/main/java/com/example/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/example/jpa/repositories/AuthorRepository.java
@@ -3,13 +3,14 @@ package com.example.jpa.repositories;
 import com.example.jpa.models.Author;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface AuthorRepository extends JpaRepository<Author,Integer> {
+public interface AuthorRepository extends JpaRepository<Author,Integer> , JpaSpecificationExecutor<Author> {
     @Modifying
     @Transactional
     @Query("update Author a set a.age=:age where a.id=:id")
